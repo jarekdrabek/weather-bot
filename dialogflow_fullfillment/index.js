fea9c66b83bea9e973f7ba483969785e8187430a
Jaroslaw Drabek <jaroslaw.drabek@ocado.com>
2019-02-25 17:52:22 +0100

    files3

--- dialogflow_fullfillment/index.js
+++ dialogflow_fullfillment/index.js
@@ -1,23 +1,26 @@
 'use strict';
 
 const http = require('http');
+const https = require('https');
 const functions = require('firebase-functions');
 
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest((req, res) => {
   	let date = req.body.queryResult.parameters.date;
     let city = req.body.queryResult.parameters.city;
-    callWeatherApi(city, date).then(output => res.json({"fulfillmentText":output}));
+    callGetCoordinates(city).then(output =>{
+        callWeatherApi(output.cityName, output.coordinates, date).then(output =>
+            res.json({"fulfillmentText":output}));
+    });
 });
 
-function callWeatherApi (city, date) {
-
+function callWeatherApi (cityName, coordinates, date) {
   const host = 'api.worldweatheronline.com';
   const wwoApiKey = '3772aabe522543cbbfa150114191402';
 
   return new Promise((resolve, reject) => {
     let day_date = date.substring(0,10);
     let path = '/premium/v1/weather.ashx?format=json&num_of_days=1' +
-      '&q=' + encodeURIComponent(city) + '&key=' + wwoApiKey + '&date=' + day_date;
+      '&q=' + coordinates + '&key=' + wwoApiKey + '&date=' + day_date;
     console.log('API Request: ' + host + path);
 
     http.get({host: host, path: path}, (res) => {
@@ -32,7 +35,7 @@ function callWeatherApi (city, date) {
         let weather_description = the_day_at_noon.weatherDesc[0].value;
 
         let output = `${weather_description} with the temperature of ${temperature}°C
-        in ${location} on ${day_date}`;
+        in ${cityName} on ${day_date}`;
 
         console.log(output);
         resolve(output);
@@ -42,5 +45,36 @@ function callWeatherApi (city, date) {
         reject();
       });
     });
+  });
+}
+
+function callGetCoordinates(cityName){
+
+  let google_host = 'https://maps.googleapis.com';
+  let googleApiKey = 'AIzaSyBj2ZABDNF1VhG9zCI__lF4DttQ_I5Zkuw';
+  let path = "/maps/api/geocode/json?address="+encodeURIComponent(cityName)+"&key="+googleApiKey;
+  console.log('Google API Request: ' + google_host + path);
+
+  return new Promise((resolve, reject) => {
+      https.get(google_host+path, (res) => {
+          let body = '';
+          res.on('data', function (chunk) {
+              body += chunk;
+          });
+          res.on('end', function () {
+              console.log('Response: ' + body);
+              let response = JSON.parse(body);
+              let lat = response.results[0].geometry.location.lat;
+              let lng = response.results[0].geometry.location.lng;
+              let coordinates = lat + "," + lng;
+              console.log("Coordinates: " + coordinates);
+              resolve({"cityName":decodeURIComponent(cityName), "coordinates":coordinates});
+          });
+          res.on('error', (error) => {
+              console.log(`Error calling Google Geo API: ${error}`);
+              reject();
+          });
+
+      });
   });
 }
